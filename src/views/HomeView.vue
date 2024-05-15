0c678d4016810fa1a254956aedad123580b6b703
Magnus <mp-se@noreply.github.com>
2024-05-15 07:37:31 +0200

    Update first page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,19 @@
     <p class="h3">Home</p>
     <hr>
     <p>
-      Welcome to BrewLogger, an simple tool for collecting gravity data from GravityMon and iSpindle devices. 
+      Welcome to BrewLogger, an simple gravity logging tool and device management. Support the following projects:
+    </p>
+    <p>
+      <ul>
+        <li>Gravitymon, Gravity logging</li>
+        <li>Kegmon (WIP, Pour loggin)</li>
+        <li>Pressuremon (WIP, pressure logging)</li>
+        <li>BrewPI ESP, Temperature control, Display</li>
+        <li>iSpindel, Gravity logging</li>
+      </ul> 
+    </p>
+    <p>
+      Integrates with brewfather to import batches.
     </p>
   </div>
 </template>
