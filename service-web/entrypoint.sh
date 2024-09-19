1d418da86f540cbb518affcfb06bacf39e55a5ca
Magnus <mp-se@noreply.github.com>
2024-09-19 13:01:08 +0200

    Fixed env for vite

--- service-web/entrypoint.sh
+++ service-web/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 echo "Starting container ${API_KEY}"
-echo "window.VUE_APP_TOKEN='${API_KEY}'" > /usr/share/nginx/html/env-config.js
+echo "window.VITE_APP_TOKEN='${API_KEY}'" > /usr/share/nginx/html/env-config.js
 nginx -g "daemon off;"
