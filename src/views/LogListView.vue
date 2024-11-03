56e9a8df2faf676fd3b42a76f04394156b12a1d9
Magnus <mp-se@noreply.github.com>
2024-11-03 10:17:03 +0100

    Adding sorting to systemlog

--- src/views/LogListView.vue
+++ src/views/LogListView.vue
@@ -12,8 +12,22 @@
       <table class="table table-striped">
         <thead>
           <tr>
-            <th scope="col" class="col-sm-2">Date</th>
-            <th scope="col" class="col-sm-2">Module</th>
+            <th scope="col" class="col-sm-2">
+              <div :class="sortedClass('timestamp')">
+                Date&nbsp;
+                <a class="icon-link icon-link-hover" @click="sortLogList('timestamp', 'date')">
+                  <i :class="sortedIconClass"></i>
+                </a>
+              </div>
+            </th>
+            <th scope="col" class="col-sm-2">
+              <div :class="sortedClass('module')">
+                Module&nbsp;
+                <a class="icon-link icon-link-hover" @click="sortLogList('module', 'str')">
+                  <i :class="sortedIconClass"></i>
+                </a>
+              </div>
+            </th>
             <th scope="col" class="col-sm-7">Message</th>
             <th scope="col" class="col-sm-1">Code</th>
           </tr>
@@ -34,11 +48,55 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { global } from '@/modules/pinia'
 import { logDebug } from '@/modules/logger'
 
 const logList = ref(null)
+
+const sorting = ref({ column: 'timestamp', type: 'date', order: false })
+
+function sortedClass(column) {
+  logDebug('LogListView.sortedClass()', column)
+  if (column == sorting.value.column) return 'text-primary'
+  return ''
+}
+
+const sortedIconClass = computed(() => {
+  return 'bi ' + (sorting.value.order ? 'bi-sort-alpha-down' : 'bi-sort-alpha-up')
+})
+
+function sortLogList(column, type) {
+  // Type: str, num, date
+  logDebug('LogListView.sortLogList()', column, type)
+
+  sorting.value.column = column
+  sorting.value.type = type
+  sorting.value.order = !sorting.value.order
+  applySortLogList()
+}
+
+function applySortLogList() {
+  logDebug('LogListView.applySortLogList()')
+
+  if (sorting.value.order) {
+    if (sorting.value.type == 'str')
+      logList.value.sort((a, b) => a[sorting.value.column].localeCompare(b[sorting.value.column]))
+    else if (sorting.value.type == 'date')
+      logList.value.sort(
+        (a, b) => Date.parse(a[sorting.value.column]) - Date.parse(b[sorting.value.column])
+      )
+    else logList.value.sort((a, b) => a[sorting.value.column] - b[sorting.value.column])
+  } else {
+    if (sorting.value.type == 'str')
+      logList.value.sort((a, b) => b[sorting.value.column].localeCompare(a[sorting.value.column]))
+    else if (sorting.value.type == 'date')
+      logList.value.sort(
+        (a, b) => Date.parse(b[sorting.value.column]) - Date.parse(a[sorting.value.column])
+      )
+    else logList.value.sort((a, b) => b[sorting.value.column] - a[sorting.value.column])
+  }
+}
 
 onMounted(() => {
   logDebug('LogListView.onMounted()')
@@ -63,6 +121,7 @@ function updateLogList() {
     })
     .then((json) => {
       logList.value = json
+      applySortLogList()
       global.disabled = false
     })
     .catch(() => {
