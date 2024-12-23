ca5a68868bae83198ed977bbc1d3336b35c747cc
Magnus <mp-se@noreply.github.com>
2024-12-23 14:34:30 +0100

    Adding web flashing support

--- src/views/DeviceFlashView.vue
+++ src/views/DeviceFlashView.vue
@@ -3,7 +3,8 @@
     <p class="h3">Flash devices</p>
     <hr />
     <p class="fw-normal">
-      Here you can flash your devices using the ESP Web Flasher. Note that flashing will do a FULL ERASE!
+      Here you can flash your devices using the ESP Web Flasher. Note that flashing will do a FULL
+      ERASE!
     </p>
 
     <div class="row">
@@ -31,27 +32,58 @@
       <esp-web-install-button :manifest="manifestUrl"></esp-web-install-button>
       <p>&nbsp;</p>
       <p class="fw-normal">
-        Some devices might need to be put in flash mode before flashing will work. Hold in EN and then do a reset and release EN should put the device in flashing mode.
+        Some devices might need to be put in flash mode before flashing will work. Hold in EN and
+        then do a reset and release EN should put the device in flashing mode.
       </p>
-      <p class="fw-normal">
-        Using manfifest: {{ manifestUrl }}
-      </p>
-      <p class="fw-normal">
-        Powered by <b>ESP Web Tools</b>
-      </p>
-  </div>
+      <p class="fw-normal">Using manfifest: {{ manifestUrl }}</p>
+      <p class="fw-normal">Powered by <b>ESP Web Tools</b></p>
+    </div>
+
+    <div class="col-md-12" v-if="softwareVer != ''">
+      <p class="fw-normal">Software Version: {{ softwareVer }}</p>
+    </div>
+
+    <div class="col-md-12" v-if="supportedBoards.length">
+      <p class="fw-normal">List of supported boards:</p>
+      <ul>
+        <li class="fw-normal" v-for="(b, index) in supportedBoards" :key="index">{{ b }}</li>
+      </ul>
+    </div>
 
   </div>
 </template>
 
 <script setup>
 import { computed, ref, watch } from 'vue'
+import { global } from '@/modules/pinia'
+import { storeToRefs } from 'pinia'
+import { logDebug, logError } from '@/modules/logger'
+
+const { disabled } = storeToRefs(global)
 
 const software = ref('')
+const softwareVer = ref('')
 const variant = ref('')
+const supportedBoards = ref([])
+
+watch(disabled, () => {
+  logDebug('App.watch(disabled)')
+
+  if (global.disabled) document.body.style.cursor = 'wait'
+  else document.body.style.cursor = 'default'
+})
 
 watch(software, () => {
-  variant.value = ''
+  if (software.value == 'gravitymon-gateway' || software.value == 'victron-receiver') {
+    variant.value = '_lolin'
+  }
+  else {
+    variant.value = ''
+    softwareVer.value = ''
+    supportedBoards.value = []
+  }
+
+  parseManifest()  
 })
 
 const manifestUrl = computed(() => {
@@ -59,6 +91,39 @@ const manifestUrl = computed(() => {
     window.location.origin + '/firmware/' + software.value + '/manifest' + variant.value + '.json'
   )
 })
+
+watch(variant, () => {
+  parseManifest()  
+})
+
+async function parseManifest() {
+  logDebug("App.parseManifest()", manifestUrl.value)
+
+  fetch(manifestUrl.value, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    signal: AbortSignal.timeout(2000)
+  })
+    .then((res) => {
+      global.disabled = false
+      if (res.status != 200) throw res
+
+      return res.json()
+    })
+    .then((json) => {
+      logDebug('App.parseManifest()', json)
+
+      softwareVer.value = json.version
+      supportedBoards.value = []
+
+      json.builds.forEach(b => {
+        supportedBoards.value.push(b.board)
+      })
+    })
+    .catch((err) => {
+      logError('App.parseManifest()', err)
+    })
+}
 
 const softwareOptions = ref([
   { label: '- unknown -', value: '' },
