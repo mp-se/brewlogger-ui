4834e9921726d236bb318a91ffe37629eb676247
Magnus <mp-se@noreply.github.com>
2025-09-12 09:40:31 +0200

    Update debug page for self_test

--- src/views/SupportView.vue
+++ src/views/SupportView.vue
@@ -3,7 +3,11 @@
     <p></p>
     <p class="h3">Support</p>
     <hr />
+    <p class="h5">Log status</p>
     <pre>{{ JSON.stringify(logData, null, 2) }}</pre>
+    <hr />
+    <p class="h5">BLE status</p>
+    <pre>{{ JSON.stringify(bleData, null, 2) }}</pre>
   </div>
 </template>
 
@@ -13,6 +17,7 @@ import { global } from '@/modules/pinia'
 import { logDebug, logError } from '@/modules/logger'
 
 const logData = ref({})
+const bleData = ref({})
 
 onMounted(() => {
   logDebug('SupportView.onMounted()')
@@ -37,6 +42,19 @@ onMounted(() => {
           value = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}`
         }
         logData.value[deviceid][attribute] = value
+      })
+
+      json.ble.forEach((entry) => {
+        const [, deviceid, attribute] = entry.name.split('_')
+        if (!bleData.value[deviceid]) bleData.value[deviceid] = {}
+        let value = entry.value
+        if (attribute === 'last') {
+          // Convert to 'YYYY-MM-DD HH:mm' format (assume value is a Unix timestamp in seconds)
+          const date = new Date(value * 1000)
+          const pad = (n) => n.toString().padStart(2, '0')
+          value = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}`
+        }
+        bleData.value[deviceid][attribute] = value
       })
 
       logDebug('SupportView.onMounted()', json)
