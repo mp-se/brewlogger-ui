bb1b3fa50759c018e5c2b246fa9a142bf291d23a
Magnus <mp-se@noreply.github.com>
2025-12-31 15:52:52 +0100

    Pin meny to top

--- src/components/BsMenuBar.vue
+++ src/components/BsMenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
     <div class="container-fluid align-center">
       <button
         class="navbar-toggler"
